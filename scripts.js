c2c5a390888f3716ba126fdde716e51481f73345
Gabriel Ribas Ferreira de Almeida <gabrielribasfal@gmail.com>
2025-04-10 10:19:17 -0300

    styles: adicionando responsividade aos cards

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-hamburger').addEventListener('click', function() {
+    const menuList = document.getElementById('menu_list');
+    menuList.classList.toggle('active');
+});
